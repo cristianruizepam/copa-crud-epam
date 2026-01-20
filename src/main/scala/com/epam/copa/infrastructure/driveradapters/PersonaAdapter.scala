8ed718c180ea33599596b42feb39607ebd1809c3
cristianruizepam <cristian_ruiz@epamneoris.com>
2026-01-20 15:37:21 -0500

    Se crea metodo adaptador para el adapter y convierte

--- src/main/scala/com/epam/copa/infrastructure/driveradapters/PersonaAdapter.scala
+++ src/main/scala/com/epam/copa/infrastructure/driveradapters/PersonaAdapter.scala
@@ -1,6 +1,15 @@
 package com.epam.copa
 package com.epam.copa.infrastructure.driveradapters
+import com.epam.copa.com.epam.copa.domain.gateway.PersonaGateway
+import  com.epam.copa.com.epam.copa.domain.model.PersonaModel
+import com.epam.copa.infrastructure.driveradapters.inmemory.PersonaInMemory
+import com.epam.copa.infrastructure.driveradapters.mapper.PersonaMapper
 
-class PersonaAdapter {
+class PersonaAdapter extends  PersonaGateway{
 
+  override def findAll(): List[PersonaModel] = PersonaInMemory.findAll()
+    .map(PersonaMapper.toModel)
+
+  override def findById(id: String): Option[PersonaModel] = PersonaInMemory
+    .findById(id).map(PersonaMapper.toModel)
 }
